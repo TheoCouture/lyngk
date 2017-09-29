3eef63b27a8cc16778a18405d56be3f7d7a46e9c
TheoCouture <theo.couture@gmail.com>
2017-09-29 15:44:17 +0200

    Test Story 2

--- test/test.js
+++ test/test.js
@@ -4,7 +4,9 @@ var LyngkTestCase = TestCase("LyngkTestCase");
 
 
 LyngkTestCase.prototype.testStory1 = function (){
-    var coordinates = new Lyngk.Coordinates('A',1);
+    var coordinates = new Lyngk.Coordinates('B',2);
 
     assertFalse(coordinates.is_valid());
+
+    assertEquals(coordinates.is_43_Coordinates(),43);
 }

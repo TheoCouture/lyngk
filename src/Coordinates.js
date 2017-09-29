9f0b9156afe7f0a6cff4379175df2627ff8a501e
TheoCouture <theo.couture@gmail.com>
2017-09-29 15:39:45 +0200

    Code IsValid Story 1

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,4 +1,37 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
+    var colonne;
+    var ligne;
+    var grid;
+
+     var init = function (c , l)
+    {
+
+        colonne = c.charCodeAt(0);
+        colonne = colonne-65;
+        ligne = l-1;
+
+        grid = [
+        [0,0,1,0,0,0,0,0,0],
+        [0,1,1,1,1,0,0,0,0],
+        [1,1,1,1,1,1,1,0,0],
+        [0,1,1,1,1,1,1,0,0],
+        [0,1,1,1,1,1,1,1,0],
+        [0,0,1,1,1,1,1,1,0],
+        [0,0,1,1,1,1,1,1,1],
+        [0,0,0,1,1,1,1,1,0],
+        [0,0,0,0,0,0,1,0,0]
+        ];
+    }
+
+    init(c,l);
+
+
+
+    this.is_valid = function() {
+        return (grid[colonne][ligne] == 1);
+    }
+
+
 };

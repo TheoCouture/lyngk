c76dd3246365c006fac688099290c5b852352481
TheoCouture <theo.couture@gmail.com>
2017-10-12 18:41:08 +0200

    Test Story 3 -> Modification Test Unitaire

--- test/test.js
+++ test/test.js
@@ -7,16 +7,16 @@ LyngkTestCase.prototype.testStory1 = function (){
     var coordinates = new Lyngk.Coordinates('A',1);
 
     assertFalse(coordinates.is_valid());
-}
+};
 
 LyngkTestCase.prototype.testStory2 = function (){
     var coordinates = new Lyngk.Coordinates('B',2);
 
     assertEquals(43,coordinates.is_43_Coordinates());
-}
+};
 
 LyngkTestCase.prototype.testStory3 = function (){
     var coordinates = new Lyngk.Coordinates('B',2);
 
-    assertTrue(coordinates.is_Coordinates_Syntax_Valid("B2"));
-}
+    assertTrue(coordinates.is_Coordinates_in_String_Format("B2"));
+};

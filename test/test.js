df5a665fdd4b737e1fc32de6be042ac4d8d21764
TheoCouture <theo.couture@gmail.com>
2017-10-20 11:14:53 +0200

    Test Story 17

--- test/test.js
+++ test/test.js
@@ -252,5 +252,21 @@ LyngkTestCase.prototype.testStory16 = function (){
     assertEquals(Lyngk.State.VACANT,(MyEngine.Get_Intersection(coordinates2.Hash()).Get_State()));
 };
 
+LyngkTestCase.prototype.testStory17 = function (){
+
+    var MyEngine = new Lyngk.Engine();
+    var coordinates = new Lyngk.Coordinates('A',3);
+    var coordinates2 = new Lyngk.Coordinates('B',3);
+    var coordinates3 = new Lyngk.Coordinates('B',2);
+
+    MyEngine.Move_Pieces(coordinates.Hash(),coordinates2.Hash());
+    var colorB3 = MyEngine.Get_Intersection_Color(coordinates2.Hash());
+    MyEngine.Move_Pieces(coordinates2.Hash(),coordinates3.Hash());
+    var colorB2 = MyEngine.Get_Intersection_Color(coordinates3.Hash())
+
+
+    assertFalse(MyEngine.Move_Pieces(coordinates3.Hash(),coordinates2.Hash()));
+};
+
 
 
